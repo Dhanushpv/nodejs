0e2bb178f27092860fd11a1be12b47c3cc0c8567
dhanushpvenkitesh <dhanushpvenkitesh@gmail.com>
2024-08-21 11:23:21 +0530

    first

---
+++ server/hi.js
@@ -0,0 +1,16 @@
+const http =('htp');
+const PORT = 3000;
+// const url =require('url');
+// const fs = require('fs')
+
+
+const server =http.createServer((req, res)=>{
+const req_url = req.url;
+
+});
+
+server.listen(PORT ,() =>{
+    console.log(`sever running at runnig at http://localhost:${PORT}`);
+
+
+});
